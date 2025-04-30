482637d91a90cb3530efa94a78d81c7f984985f5
Leonid Belyaev <lbeliaev@gmail.com>
2025-04-30 13:56:56 -0700

    initial commit

--- src/main.rs
+++ src/main.rs
@@ -33,7 +33,7 @@ async fn main() -> Result<()> {
     let args = Args::parse();
 
     let client = RpcClient::new_with_commitment(
-        "http://localhost:8899".to_string(),
+        "https://rpc.testnet.x1.xyz".to_string(),
         CommitmentConfig::processed(),
     );
 
